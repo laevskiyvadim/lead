685080baf110857effd7e28c370f67894df06312
vadim.laevskiy77 <vadim.laevskiy77@gmail.com>
2022-06-07 12:54:36 +0300

    a

--- src/views/Content/index.js
+++ src/views/Content/index.js
@@ -24,7 +24,7 @@ export const Content = defineComponent({
     });
 
     onUnmounted(() => {
-      window.removeEventListener("resize");
+      window.removeEventListener("resize", resize);
     });
 
     const mainCls = () =>
